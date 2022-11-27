f17775bba96a7e2928d24bc5f4bf21b7e5e9b78d
des-808 <des-808@inbox.ru>
2022-11-27 14:08:35 +0300

    Добавьте файлы проекта.

---
+++ igra/igra.cpp
@@ -0,0 +1,38 @@
+﻿// igra.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+void printToConsole(string s);
+
+
+
+
+
+
+class igrok {
+	int factory = 2;
+	int automated_factory = 0;
+	int ESM = 4; 
+	int EGP = 2;//units of finished products
+	long nal = 10000;
+
+
+
+
+
+};
+
+
+
+
+void main()
+{
+	printToConsole("Hello World!");
+}
+
+void printToConsole(string s) {
+
+	cout << s << endl;
+}
